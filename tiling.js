50f835506bc69efc5e33befc7f5d5130fb19588c
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-12-22 16:47:50 +0100

    Add options to isolate monitors and hide empty spaces

--- tiling.js
+++ tiling.js
@@ -1772,9 +1772,7 @@ class Spaces extends Map {
         TopBar.fixTopBar();
         const scale = 0.9;
         let space = this.spaceOf(workspaceManager.get_active_workspace());
-        let mru = [...this.stack];
-        this.monitors.forEach(space => mru.splice(mru.indexOf(space), 1));
-        mru = [space, ...mru];
+        let mru = this._getMru();
 
         if (Main.panel.statusArea.appMenu)
             Main.panel.statusArea.appMenu.container.hide();
@@ -1844,6 +1842,22 @@ class Spaces extends Map {
         }
     }
 
+    _getMru() {
+        let mru = [...this.stack];
+        let activeSpace = this.spaceOf(workspaceManager.get_active_workspace());
+        if (prefs.isolate_monitors) {
+            mru = mru.filter(
+                (space) => space !== activeSpace && space.monitor === activeSpace.monitor,
+            );
+        } else {
+            this.monitors.forEach((space) => mru.splice(mru.indexOf(space), 1));
+        }
+        if (!prefs.show_empty_spaces) {
+            mru = mru.filter((space) => space.length > 0);
+        }
+        return [activeSpace, ...mru];
+    }
+
     selectStackSpace(direction, move) {
 
         // if in sequence preview do not run stack preview
@@ -1852,11 +1866,7 @@ class Spaces extends Map {
         }
 
         const scale = 0.9;
-        let space = this.spaceOf(workspaceManager.get_active_workspace());
-        let mru = [...this.stack];
-
-        this.monitors.forEach(space => mru.splice(mru.indexOf(space), 1));
-        mru = [space, ...mru];
+        let mru = this._getMru()
 
         if (!inPreview) {
             this._initWorkspaceStack();
